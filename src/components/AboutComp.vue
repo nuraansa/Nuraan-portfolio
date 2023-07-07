28dba9232c4fac3f0bd2164cc9a8d8e5878cb647
nuraansa <nuraansa@gmail.com>
2023-07-07 15:06:01 +0200

    update

--- src/components/AboutComp.vue
+++ src/components/AboutComp.vue
@@ -1,15 +1,16 @@
 <template>
     <div class="bg">
+        <div id="about"></div>
         <div class="container about">
             <h1 class="display-1 text-center p-5" data-aos="zoom-in">Welcome to my portfolio!</h1>
             <div class="container text-center p-2">
-                <div class="row">
+                <div class="row top-row">
                     <div class="col-5">
                         <!-- <h2>I'm Nuraan Samodien</h2> -->
                         <img class="rounded shadow" id="me" src="https://i.postimg.cc/y8WsnmxC/Screenshot-2023-07-05-at-6-07-02-PM-removebg-preview.png" alt="me" style="width: 18rem">
                     </div>
-                    <div class="col-6">
-                        <h1>About Me</h1> <hr>
+                    <div class="col-5">
+                        <h1  data-aos="zoom-in">About Me</h1> <hr>
                         <p class="about-info lh-base"> Hello World! My name is Nuraan Samodien Davids.
                             I enjoy acquiring new knowledge and mastering new skills. <br> My interest in computers
                             sparked
@@ -28,7 +29,7 @@
                 </div>
             </div>
 
-            <h1 class="p-5">Hobbies & Interests</h1>
+            <h1 class="p-5" data-aos="zoom-in">Hobbies & Interests</h1>
             <div class="container pb-3">
                 <div class="row img-div g-3">
                     <div class="col" v-for="item in hobbies" :key="item.id">
@@ -48,7 +49,7 @@
                     </div>
                 </div>
             </div>
-            <router-link to="/resume" id="res" class="btn btn-light m-3 p-3 ">View Resume</router-link>
+            <router-link to="/resume" exact v-scroll-to="{ el:'#resume' }" id="res" class="btn btn-light m-3 p-3 ">View Resume</router-link>
         </div>
     </div>
 </template>
@@ -135,6 +136,30 @@ h1 {
     color:  #5E8B7E;
     border: 2px solid #5E8B7E;
     text-decoration: none;
+}
+
+.col{
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+@media screen and (max-width: 770px) {
+    .top-row{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+    }
+
+    .col-5{
+        width: 40rem;
+    }
+
+    .col-5 img{
+        width: 100%;
+    }
 }
 
 </style>
--- src/main.js
+++ src/main.js
@@ -9,5 +9,6 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 //AOS
 import AOS from 'aos'
 import 'aos/dist/aos.css'
+import vueScrollTo from 'vue-scrollto'
 
-createApp(App).use(store).use(router).use(AOS.init()).mount('#app')
+createApp(App).use(store).use(router).use(AOS.init()).use(vueScrollTo).mount('#app')
